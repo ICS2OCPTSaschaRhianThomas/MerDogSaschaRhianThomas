f4b8c713ef35e80eea2d3a212e0f5290e3d61031
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-12-10 13:57:45 -0500

    thomas by himself fixed everything

--- credits_screen.lua
+++ credits_screen.lua
@@ -101,6 +101,11 @@ function scene:create( event )
 
     -- Associating display objects with this scene 
     sceneGroup:insert( bkg_image )
+    sceneGroup:insert( sascha )
+    sceneGroup:insert( thomas )
+    sceneGroup:insert( rhian )
+    sceneGroup:insert( kaitlyn )
+    sceneGroup:insert( phoebe )
 
     -- Send the background image to the back layer so all other objects can be on top
     bkg_image:toBack()
