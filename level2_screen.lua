903be676d468da44fda59b28f97880b710d9eecc
saschamotz <sascha.motz@ocsbstudent,ca>
2018-12-11 14:23:23 -0500

    sound

--- level2_screen.lua
+++ level2_screen.lua
@@ -12,6 +12,12 @@
 -- INITIALIZATIONS
 -----------------------------------------------------------------------------------------
 
+----------------------------------------------------------------------------------------
+    -- SOUNDS
+    ----------------------------------------------------------------------------------------
+ local level1Sound = audio.loadSound( "Sounds/Level1.mp3" )
+local level1SoundChannel
+    ----------------------------------------------------------------------------------------
 
 -- Use Composer Library
 local composer = require( "composer" )
@@ -37,6 +43,7 @@ local scene = composer.newScene( sceneName )
 
 -- The local variables for this scene
 local bkg
+
 
 -- determine the range for the numbers to add
 local MIN_NUM = 1
@@ -98,21 +105,19 @@ local alreadyClickedAnswer = false
 -- SOUND
 -----------------------------------------------------------------------------------------
 -- bkg game music
-local bkgMusic = audio.loadSound("Sounds/bkgMusic.mp3" ) 
+local level1Sound = audio.loadSound("Sounds/Level1.mp3" ) 
 -- Setting a variable to an mp3 file
-local bkgMusicChannel
+local level1Channel
 
 -- corect sound
-local correctSound = audio.loadSound("Sounds/CorrectAnswer.mp3" ) 
+local correctSound = audio.loadSound("Sounds/Correct.mp3" ) 
 -- Setting a variable to an mp3 file
 local correctSoundChannel
 
 -- incorect sound
-local incorrectSound = audio.loadSound("Sounds/WrongBuzzer.mp3" ) 
+local incorrectSound = audio.loadSound("Sounds/Incorrect.mp3" ) 
 -- Setting a variable to an mp3 file
 local incorrectSoundChannel
-
-
 
 -----------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
@@ -121,7 +126,7 @@ local incorrectSoundChannel
 local function DetermineAnswers()
     -- calculate the correct answer as well as the wrong answers
     answer = firstNumber + secondNumber
-    wrongAnswer1 = answer + math.random(1,3)
+    wrongAnswer1 = answer - math.random(1,3)
     wrongAnswer2 = answer + math.random(4,8)
     wrongAnswer3 = answer + math.random(9,13)
 end
@@ -491,7 +496,8 @@ function scene:show( event )
     local phase = event.phase
 
     -- play bkg music
-    bkgMusicChannel = audio.play(bkgMusic)
+    level1SoundChannel = audio.play(level1Sound)
+
     -----------------------------------------------------------------------------------------
 
     if ( phase == "will" ) then
@@ -527,6 +533,8 @@ function scene:hide( event )
 
     if ( phase == "will" ) then
 
+        audio.stop()
+
 
         -- Called when the scene is on screen (but is about to go off screen).
         -- Insert code here to "pause" the scene.
@@ -554,7 +562,11 @@ function scene:destroy( event )
     -- Called prior to the removal of scene's view ("sceneGroup").
     -- Insert code here to clean up the scene.
     -- Example: remove display objects, save state, etc.
+
+    audio.stop(bkgMusic)
+
 end
+
 
 -----------------------------------------------------------------------------------------
 -- EVENT LISTENERS
