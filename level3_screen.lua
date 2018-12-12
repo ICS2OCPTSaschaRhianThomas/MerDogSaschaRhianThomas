3ae7addce981890052c43e3b21e7bc5c36007e64
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-12-12 13:34:39 -0500

    level 3

--- level3_screen.lua
+++ level3_screen.lua
@@ -121,6 +121,7 @@ local incorrectSoundChannel
 local function DetermineAnswers()
     -- calculate the correct answer as well as the wrong answers
     answer = firstNumber + secondNumber
+    answer = firstNumber - secondNumber
     wrongAnswer1 = answer + math.random(1,3)
     wrongAnswer2 = answer + math.random(4,8)
     wrongAnswer3 = answer + math.random(9,13)
