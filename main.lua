c5e87b08cc17e3f34f58a965b4ff8926d13a14a2
Immaculata <immaculata@Immaculatas-iMac.local>
2019-01-09 14:50:11 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,11 +17,8 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-
-
 composer.gotoScene( "splash_screen" )
 
-composer.gotoScene( "level1_screen" )
 
 
 
